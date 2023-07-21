ab5059f91b0f682b80396f1779877ef9f0bcd803
Rohith patel <rohithgithub999@gmail.com>
2023-07-21 17:28:02 +0000

    Post Api book

--- myapp/index.js
+++ myapp/index.js
@@ -5,6 +5,8 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
+app.use(express.json());
+//console.log(app.use(express.json()));
 const dbPath = path.join(__dirname, "goodreads.db");
 
 let db = null;
@@ -62,4 +64,43 @@ app.get("/books/:bookId/", async (request, response) => {
   */
   console.log("Nodemon is working properly");
   response.send(booksArray);
+});
+
+app.post("/books/", async (request, response) => {
+  const requestBody = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+  const addBookQuery = `
+    INSERT INTO
+      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+    VALUES
+      (
+        '${title}',
+         ${authorId},
+         ${rating},
+         ${ratingCount},
+         ${reviewCount},
+        '${description}',
+         ${pages},
+        '${dateOfPublication}',
+        '${editionLanguage}',
+         ${price},
+        '${onlineStores}'
+      );`;
+  const dbResponse = await db.run(addBookQuery);
+  console.log(dbResponse);
+  const BookId = dbResponse.lastID;
+  response.send({ bookId: bookId });
+  //console.log(requestBody);
 });
