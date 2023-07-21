d4268109fbe8d009b4575b88d471e28e2f5edca8
Rohith patel <rohithgithub999@gmail.com>
2023-07-21 18:56:15 +0000

    put book api

--- myapp/index.js
+++ myapp/index.js
@@ -104,3 +104,44 @@ app.post("/books/", async (request, response) => {
   response.send({ bookId: bookId });
   //console.log(requestBody);
 });
+//update book api
+app.put("/books/:bookId/", async (request, response) => {
+  const { bookId } = request.params;
+  //console.log(request.params);//outpu:{ bookId: '41' }
+  //console.log(requestBody);
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price= ${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`;
+  const dbResponse = await db.run(updateBookQuery);
+  console.log("updated successfully");
+  response.send(dbResponse);
+});
